dc4005d2eab67b7d6cf319ad2da2613db2768512
yxyyyyyyyy <3436771164@qq.com>
2025-01-08 22:15:52 +0800

    基础框架搭建,引入主题库

--- blogfront/src/App.vue
+++ blogfront/src/App.vue
@@ -3,6 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+
+  <yk-space>
+    <yk-button>主要按钮</yk-button>
+    <yk-button type="secondary">次要按钮</yk-button>
+    <yk-button type="outline">线框按钮</yk-button>
+  </yk-space>
+
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -11,10 +18,15 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <div class="self">qqqq</div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style scoped lang="less">
+@color : red;
+.self{
+  color: @scolor;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
--- blogfront/vite.config.ts
+++ blogfront/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { YikeResolver } from '@yike-design/resolver' // https://vitejs.dev/config/ export default
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({ resolvers: [YikeResolver()] }),
+    Components({ resolvers: [YikeResolver({ sideEffect: true })] }),
+  ],
+  css: {
+    // css预处理器，方便直接使用组件样式变量（按需配置）
+    preprocessorOptions: {
+      less: {
+        charset: false,
+        additionalData:
+          '@import "@yike-design/ui/es/components/styles/basis.less";',
+      },
+    },
+  },
 })
