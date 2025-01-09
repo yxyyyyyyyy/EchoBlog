d59ccd833391ba319790fd3cadddc587c15fb6a5
yxyyyyyyyy <3436771164@qq.com>
2025-01-09 18:57:32 +0800

    标题组件开发

---
+++ blogfront/src/components/TopTitleBar.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="top-title">
+        <yk-title :level="3" style="margin: 0'">{{ props.name }}</yk-title>
+        <slot name="custom"></slot>
+        <yk-space size="s" v-if="isSeacher">
+            <yk-button type="secondary" v-show="SearchContent" @click="cancelSearch">取消搜索</yk-button>
+            <yk-input-search style="width: 320px" placeholder="请输入标题/简介..." v-model="SearchContent"
+            @search="search">
+                <template #suffix>
+                    <yk-button type="secondary">
+                        <IconSearchOutline />
+                    </yk-button>
+                </template>
+            </yk-input-search>
+        </yk-space>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+
+const SearchContent = ref('')
+
+type titleProps = {
+    name: string;
+    isSeacher:boolean;
+}
+const props = withDefaults(defineProps<titleProps>(), {
+    name: '总览',
+    isSeacher:true
+})
+
+const emit= defineEmits(['search'])
+// 搜索事件
+const search = () => {
+    emit('search',SearchContent.value)
+}
+// 取消搜索
+const cancelSearch = () => {
+    emit('search','')
+    SearchContent.value=''
+}
+</script>
+
+<style lang="less" scoped>
+.top-title {
+    display: flex;
+    justify-content:space-between;
+    align-items:center;
+    margin: 20px 30px;
+}
+</style>
--- blogfront/src/views/OverView.vue
+++ blogfront/src/views/OverView.vue
@@ -1,9 +1,14 @@
 <template>
-    <!-- <div>总览</div> -->
+    <TopTile :is-seacher="false" :name="childName">
+        <!-- <template #custom>
+            <yk-button>aaaa</yk-button>
+        </template> -->
+    </TopTile>
 </template>
 
 <script lang="ts" setup>
-
+import TopTile from '../components/TopTitleBar.vue'
+const childName = '总览';
 </script>
 
 <style lang="less" scoped>
